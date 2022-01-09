f22ed8a0c44ebea799614192cc75346cb3bf7f10
Maciej "Iwan" Iwanowski <maciej.iwanowski@critical.today>
2022-01-09 20:58:44 +0100

    Trying to make goreleaser work...

---
+++ build/goreleaser-hook-post.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt purge libipmctl-dev libpfm4-dev
---
+++ build/goreleaser-hook-pre.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt install libipmctl-dev libpfm4-dev
